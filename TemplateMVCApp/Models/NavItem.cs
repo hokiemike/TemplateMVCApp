eaf2f24a9290b24e40c5cd3a419b135435fadd92
Mike Starr <hokiemike@gmail.com>
2013-05-20 07:24:41 -0400

    add nav tab code

---
+++ TemplateMVCApp/Models/NavItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TemplateMVCApp.Models
+{
+    public abstract class NavItem
+    {
+        public string DisplayText { get; set; }
+        
+       
+    }
+
+    public class NavHeader : NavItem
+    {
+        
+    }
+
+    public class NavLink : NavItem
+    {
+        public string Url { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
